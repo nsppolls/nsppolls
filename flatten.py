4b4d6ea9e9841ee567b605283bd0f21144bb241b
nmondon <nicolasmondon@gmail.com>
2021-11-25 18:06:52 +0100

    Setup gh actions

---
+++ flatten.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from pathlib import Path
+import json
+import re
+
+resp = Path('presidentielle.json').read_text()
+
+nsp_polls = (pd
+    .json_normalize(
+        json.loads(resp),
+        record_path=['tours','hypotheses', 'candidats'],
+        meta=[
+            'id', 'nom_institut', 'commanditaire', 'debut_enquete', 'fin_enquete', 'echantillon', 'population',
+            ['tours', 'tour'],
+            ['tours', 'hypotheses', 'hypothese'],
+            ['tours', 'hypotheses', 'sous_echantillon'],
+        ]
+    )
+    .rename(columns={
+        'tours.tour': 'tour',
+        'tours.hypotheses.hypothese': 'hypothese',
+        'tours.hypotheses.sous_echantillon': 'sous_echantillon'
+    })
+    .fillna({
+        'parti': '',
+        'candidat': 'autre'
+    })
+    .assign(
+        parti = lambda df: df['parti'].apply(lambda p: re.sub('\[|\]|\'', '', str(p))),
+        debut_enquete = lambda df: pd.to_datetime(df['debut_enquete']),
+        fin_enquete = lambda df: pd.to_datetime(df['fin_enquete'])
+    )
+    .to_csv('presidentielle.csv', index=False, sep=',')
+)
